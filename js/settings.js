191f2dc86a852a4f1632dde98cf72f81e49a00e7
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-12-09 17:01:40 -0500

    Getting ready for Chrome store

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,18 @@
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-74212008-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButtonClick(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+
 // Time variables
 var second = 1000;
 var minute = second * 60;
@@ -35,38 +50,45 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // Listener for the number
+  // Listeners for the number
+  expirationDropdownNumber.addEventListener('click', trackButtonClick);
   expirationDropdownNumber.addEventListener('change', function() {
     saveExpirationDuration(expirationDropdownNumber.value, expirationDropdownTimeUnit.value);
   });
 
-  // Listener for the time unit
+  // Listeners for the time unit
+  expirationDropdownTimeUnit.addEventListener('click', trackButtonClick);
   expirationDropdownTimeUnit.addEventListener('change', function() {
     saveExpirationDuration(expirationDropdownNumber.value, expirationDropdownTimeUnit.value);
   });
 
-  // Listener for the whitelist
+  // Listeners for the whitelist
+  tabWhitelist.addEventListener('click', trackButtonClick);
   tabWhitelist.addEventListener('change', function() {
     chrome.storage.sync.set({'tabWhitelistTextArea': tabWhitelist.value});
   });
 
-  // Tab duration info listener
-  document.getElementById('show-tab-duration-modal').addEventListener('click', function() {
+  // Tab duration info listeners
+  document.getElementById('show-tab-duration-modal').addEventListener('click', function(e) {
     document.getElementById('tab-duration-modal').classList.add('is-active');
+    trackButtonClick(e);
   });
 
-  // Tab duration modal close listener
-  document.getElementById('tab-duration-modal-close').addEventListener('click', function() {
+  // Tab duration modal close listeners
+  document.getElementById('tab-duration-modal-close').addEventListener('click', function(e) {
     document.getElementById('tab-duration-modal').classList.remove('is-active');
+    trackButtonClick(e);
   });
 
   // Tab whitelist info listener
-  document.getElementById('show-tab-whitelist-modal').addEventListener('click', function() {
+  document.getElementById('show-tab-whitelist-modal').addEventListener('click', function(e) {
     document.getElementById('tab-whitelist-modal').classList.add('is-active');
+    trackButtonClick(e);
   });
 
   // Tab whitelist modal close listener
-  document.getElementById('tab-whitelist-modal-close').addEventListener('click', function() {
+  document.getElementById('tab-whitelist-modal-close').addEventListener('click', function(e) {
     document.getElementById('tab-whitelist-modal').classList.remove('is-active');
+    trackButtonClick(e);
   });
 });
